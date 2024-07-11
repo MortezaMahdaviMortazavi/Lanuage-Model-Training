fce49a2e24e88eb8654d9022e58be4d5e82761d5
Morteza Mahdavi <98619035+MortezaMahdaviMortazavi@users.noreply.github.com>
2024-07-11 14:08:22 +0330

    Add files via upload

--- masked_modeling/train_lightning.py
+++ masked_modeling/train_lightning.py
@@ -14,12 +14,9 @@ from accelerate import Accelerator
 from tqdm.auto import tqdm
 
 """
-python train.py --data_path combined_datasets/cleaned_sentences.csv --logger_file logs/training.log --target_column text --model_name sbunlp/fabert --output_dir MLP_TrainedModels --mlm_probability 0.2 --batch_size 4 --chunk_size 256 --num_train_epochs 30 --learning_rate 5e-5 --random_seed 42 --print_per_batch_num 100 --train_size 0.9 --test_size 0.1
-
+python train_lightning.py --data_path combined_datasets/cleaned_sentences.csv --logger_file logs/training.log --target_column text --model_name sbunlp/fabert --output_dir MLP_TrainedModels --mlm_probability 0.2 --batch_size 4 --chunk_size 256 --num_train_epochs 30 --learning_rate 5e-5 --random_seed 42 --print_per_batch_num 100 --train_size 0.9 --test_size 0.1
 
 """
-
-
 
 def parse_arguments():
     """Parse command line arguments."""
@@ -28,21 +25,21 @@ def parse_arguments():
     parser.add_argument('--target_column', type=str, required=False, help="Name of the target column in the data CSV file")
     parser.add_argument('--model_name', type=str, required=True, help="Name of the pretrained model")
     parser.add_argument('--output_dir', type=str, required=True, help="Directory to save the trained model")
-    parser.add_argument("--logger_file",type=str , required=True ,help="Log everything in the training")
+    parser.add_argument("--logger_file", type=str, required=True, help="Log everything in the training")
     parser.add_argument('--mlm_probability', type=float, default=0.2, help="Probability for masking tokens in MLM")
     parser.add_argument('--batch_size', type=int, default=32, help="Batch size for training and evaluation")
     parser.add_argument('--chunk_size', type=int, default=128, help="Chunk size for grouping texts")
     parser.add_argument('--num_train_epochs', type=int, default=3, help="Number of training epochs")
     parser.add_argument('--learning_rate', type=float, default=5e-5, help="Learning rate for the optimizer")
     parser.add_argument('--random_seed', type=int, default=42, help="Random seed for reproducibility")
-    parser.add_argument("--print_per_batch_num",type=int,default=100,help="Print loss and track training after this number of batch")
+    parser.add_argument("--print_per_batch_num", type=int, default=100, help="Print loss and track training after this number of batch")
     parser.add_argument('--train_size', type=float, default=0.9, help="Train size rate")
     parser.add_argument('--test_size', type=float, default=0.1, help="Test size rate")
     return parser.parse_args()
 
 def setup_logging(log_file_path):
     """Set up the logging configuration."""
-    os.makedirs(os.path.dirname("./" + log_file_path), exist_ok=True)
+    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
     if os.path.exists(log_file_path):
         open(log_file_path, 'w').close()
@@ -56,8 +53,6 @@ def setup_logging(log_file_path):
             logging.StreamHandler()
         ]
     )
-
-
 
 def load_csv_data(path: str, target_column: str) -> Dataset:
     """Load and prepare dataset from a CSV file."""
@@ -125,8 +120,6 @@ def load_and_prepare_data(path: str, target_column: str = None) -> Dataset:
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         raise
-
-
 
 def load_tokenizer(model_name: str = "sbunlp/fabert"):
     """
@@ -235,7 +228,7 @@ def prepare_dataloader(dataset, tokenizer, batch_size: int, mlm_probability: flo
         "masked_input_ids": "input_ids",
         "masked_attention_mask": "attention_mask",
         "masked_labels": "labels",
-        "masked_token_type_ids":"token_type_ids"
+        "masked_token_type_ids": "token_type_ids"
     })
     print(len(downsampled_dataset["train"]), len(downsampled_dataset["test"]))
 
@@ -244,87 +237,84 @@ def prepare_dataloader(dataset, tokenizer, batch_size: int, mlm_probability: flo
     
     return train_dataloader, eval_dataloader
 
-def train_model(
-        model: AutoModelForMaskedLM, 
-        train_dataloader: DataLoader, 
-        eval_dataloader: DataLoader,
-        batch_size: int, 
-        num_train_epochs: int,
-        print_per_batch_num: int, 
-        learning_rate: float, 
-        output_dir: str):
-    """
-    Train the masked language model.
-    
-    Args:
-    model (AutoModelForMaskedLM): Pretrained model for masked language modeling.
-    train_dataloader (DataLoader): Data loader for training.
-    eval_dataloader (DataLoader): Data loader for evaluation.
-    batch_size (int): Batch size.
-    num_train_epochs (int): Number of training epochs.
-    print_per_batch_num (int): Print loss every specified number of batches.
-    learning_rate (float): Learning rate for the optimizer.
-    output_dir (str): Directory to save the trained model.
+class LightingTrainer(pl.LightningModule):
+    def __init__(self, model, args):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.step_count = 0
 
-    Returns:
-    None
-    """
-    
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
-    accelerator = Accelerator()
-    model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(model, optimizer, train_dataloader, eval_dataloader)
-    
-    num_update_steps_per_epoch = len(train_dataloader)
-    
+    def forward(self, batch):
+        return self.model(**batch)
 
-    num_training_steps = num_train_epochs * num_update_steps_per_epoch
-    logging.info(f"num_training_steps: {num_training_steps}")
-    lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+    def training_step(self, batch, batch_idx):
+        outputs = self.forward(batch)
+        loss = outputs.loss
+        self.log('train_loss', loss, prog_bar=True, logger=True)
+        self.step_count += 1
+        return loss
 
-    logging.info("Starting training process...")
+    def validation_step(self, batch, batch_idx):
+        outputs = self.forward(batch)
+        loss = outputs.loss
+        self.log('val_loss', loss, prog_bar=True, logger=True)
 
-    for epoch in range(num_train_epochs):
-        model.train()
-        train_loss = 0.0
-        for idx, batch in enumerate(tqdm(train_dataloader)):
-            outputs = model(**batch)
-            loss = outputs.loss
-            accelerator.backward(loss)
-            train_loss += loss.item()
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
+        if batch_idx % 200 == 0:
+            self.validation_perplexity()
 
-            if idx % print_per_batch_num == 0:
-                print(f"Batch {idx}, Loss: {train_loss / (idx + 1)}")
+        return loss
 
-        epoch_train_result = f"Epoch {epoch} Train Loss: {train_loss / len(train_dataloader)}"
-        print(epoch_train_result)
-        logging.info(epoch_train_result)
-
-        model.eval()
+    def validation_perplexity(self):
+        self.model.eval()
         losses = []
-        for batch in tqdm(eval_dataloader):
+        val_dataloader = self.val_dataloader()
+        for batch in val_dataloader:
             with torch.no_grad():
-                outputs = model(**batch)
-            loss = outputs.loss
-            losses.append(accelerator.gather(loss.repeat(batch_size)))
+                outputs = self.forward(batch)
+                loss = outputs.loss
+                losses.append(loss)
 
-        losses = torch.cat(losses)[:len(eval_dataloader.dataset)]
-        try:
-            perplexity = math.exp(torch.mean(losses))
-        except OverflowError:
-            perplexity = float("inf")
+        avg_loss = torch.mean(torch.tensor(losses))
+        perplexity = torch.exp(avg_loss)
 
-        epoch_result = f">>> Epoch {epoch}: Perplexity: {perplexity}"
-        print(epoch)
-        logging.info(epoch_result)
+        result = f"Step {self.step_count}: Validation Perplexity: {perplexity.item()}"
+        logging.info(result)
+        print(result)
+        self.model.train()
+        return perplexity
 
-        accelerator.wait_for_everyone()
-        unwrapped_model = accelerator.unwrap_model(model)
-        unwrapped_model.save_pretrained(output_dir, save_function=accelerator.save)
-        if accelerator.is_main_process:
-            tokenizer.save_pretrained(output_dir)
+    def test_step(self, batch, batch_idx):
+        outputs = self.forward(batch)
+        loss = outputs.loss
+        self.log('test_loss', loss, prog_bar=True, logger=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.args.learning_rate)
+        num_update_steps_per_epoch = len(self.train_dataloader())
+        num_training_steps = self.args.num_train_epochs * num_update_steps_per_epoch
+        scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+        return [optimizer], [scheduler]
+
+    def train_dataloader(self):
+        return self._train_dataloader
+
+    def val_dataloader(self):
+        return self._val_dataloader
+
+    def test_dataloader(self):
+        return self._test_dataloader
+
+    @staticmethod
+    def load_weights(model, checkpoint_path):
+        weights = torch.load(checkpoint_path)
+        new_weights = {}
+        for key, value in weights.items():
+            new_key = key.replace('model.', '')  # Remove 'model' prefix
+            new_weights[new_key] = value
+        del weights
+        model.load_state_dict(new_weights)
+        print("Loading weights was successful")
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -333,22 +323,19 @@ if __name__ == "__main__":
     dataset = load_and_prepare_data(path=args.data_path, target_column=args.target_column)
     tokenizer, model = load_model_and_tokenizer(args.model_name)
     train_dataloader, eval_dataloader = prepare_dataloader(
-        dataset, 
-        tokenizer, 
-        batch_size=args.batch_size, 
-        mlm_probability=args.mlm_probability, 
-        train_size=args.train_size, 
-        test_size=args.test_size, 
-        chunk_size=args.chunk_size, 
+        dataset,
+        tokenizer,
+        batch_size=args.batch_size,
+        mlm_probability=args.mlm_probability,
+        train_size=args.train_size,
+        test_size=args.test_size,
+        chunk_size=args.chunk_size,
         seed=args.random_seed
     )
-    train_model(
-        model, 
-        train_dataloader, 
-        eval_dataloader, 
-        batch_size=args.batch_size, 
-        num_train_epochs=args.num_train_epochs, 
-        print_per_batch_num=args.print_per_batch_num,  # You can change this as needed
-        learning_rate=args.learning_rate, 
-        output_dir=args.output_dir
-    )
+    lightning_module = LightingTrainer(model, args)
+    lightning_module._train_dataloader = train_dataloader
+    lightning_module._val_dataloader = eval_dataloader
+    trainer = pl.Trainer(max_epochs=args.num_train_epochs,accelerator="cpu",strategy="auto")
+    trainer.fit(lightning_module)
+    lightning_module.model.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
